112bf452f0edd919a86c29cbdf45fa85f426a6bf
Lisa06010416 <linlisa0601@gmail.com>
2021-05-26 20:46:26 +0800

    use conda install ta-lib

--- setup.py
+++ setup.py
@@ -4,23 +4,6 @@ from setuptools import find_packages, setup
 from setuptools.command.install import install
 
 
-class MyInstall(install):
-    def run(self):
-        if platform.system().lower() == 'windows': ##?
-            os.system("wget https://download.lfd.uci.edu/pythonlibs/q4trcu4l/cp36/TA_Lib-0.4.19-cp36-cp36m-win_amd64.whl")
-            os.system("pip install TA_Lib-0.4.19-cp36-cp36m-win_amd64.whl")
-        elif platform.system().lower() == 'linux': ##?
-            os.system("wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz")
-            os.system("tar -xvf ta-lib-0.4.0-src.tar.gz")
-            os.system("cd ta-lib")
-            os.system("./configure --prefix=/usr")
-            os.system("make")
-            os.system("sudo make install")
-        else:
-            os.system("brew install ta-lib")
-        install.run(self)
-
-
 setup(
     name="financial_indicators",
     version="0.1.0",
@@ -38,6 +21,5 @@ setup(
         "Intended Audience :: Developers",
         "Environment :: MacOS X",
         "Programming Language :: Python",
-    },
-    cmdclass={'install': MyInstall},
+    }
 )
